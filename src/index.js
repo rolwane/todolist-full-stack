b906f0320031e683032df0ca948425dda02627b4
rolwane <rolwane@hotmail.com>
2022-06-26 20:18:39 -0300

    feat(index.js): initial structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+app.use(express.json());
+
+app.listen(PORT, () => global.console.log(`Server running on port ${PORT}`));
