489deb500b0bccb3bcdfbe7c669cfb63242da5f3
rolwane <rolwane@hotmail.com>
2022-06-28 16:59:50 -0300

    feat: added middleware to validate request body

---
+++ src/middlewares/userMiddleware.js
@@ -0,0 +1,22 @@
+/* eslint-disable consistent-return */
+const joi = require('joi');
+
+const validateBody = ({ body }, response, next) => {
+  const schema = joi.object({
+    name: joi.string().min(3).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(8).required(),
+  });
+
+  const { error } = schema.validate(body);
+
+  if (error) {
+    return response.status(422).json({ error: error.message });
+  }
+
+  next();
+};
+
+module.exports = {
+  validateBody,
+};
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
+const userMiddleware = require('../middlewares/userMiddleware');
 const userController = require('../controllers/userController');
 
-router.post('/user', userController.registerNewUser);
+router.post('/user', userMiddleware.validateBody, userController.registerNewUser);
 
 module.exports = router;
