be25db1c1be449126b468ed07a30119ebef7638c
rolwane <rolwane@hotmail.com>
2022-07-04 12:44:30 -0300

    feat: layout login screen

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <h1>Login Page</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section className="flex w-full h-screen bg-sky-600 items-center justify-center">
+
+      <form className="flex flex-col max-w-[340px] w-full gap-4 p-4" onSubmit={handleSubmit}>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full p-4 rounded-lg text-md font-semibold text-gray-600"
+          onChange={({ target }) => setEmail(target.value)}
+          value={email}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-4 rounded-lg text-md font-semibold text-gray-600"
+          onChange={({ target }) => setPassword(target.value)}
+          value={password}
+          required
+        />
+
+        <button
+          type="submit"
+          className="w-full p-4 rounded-lg bg-teal-500 text-white text-md font-bold"
+        >
+          Sign In
+        </button>
+
+        <Link
+          to="/asdasdad"
+          className="text-white text-center text-sm font-bold"
+        >
+          {'Don\'t have an account?'}
+        </Link>
+
+      </form>
+
+    </section>
+  );
 }
 
 export default Login;
