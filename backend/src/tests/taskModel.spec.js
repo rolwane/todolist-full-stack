98294c2eae8639ea172fd54b4fa1fd42e15d8439
rolwane <rolwane@hotmail.com>
2022-07-04 11:29:14 -0300

    feat: created test for taskModel's addNewTask function

--- backend/src/tests/taskModel.spec.js
+++ backend/src/tests/taskModel.spec.js
@@ -6,8 +6,15 @@ describe('Test taskModel', () => {
   it('Should getAllByUserId return correctly', async () => {
     connection.execute = jest.fn().mockResolvedValue([[]]);
     const response = await taskModel.getAllByUserId(0);
-    
+
     expect(response).toEqual([]);
+  });
+
+  it('Should addNewTask return correctly', async () => {
+    connection.execute = jest.fn().mockResolvedValue([{ insertId: 0 }]);
+    const response = await taskModel.addNewTask({ title: 'Estudar JS', status: 'pendente', userId: 0 });
+    
+    expect(response).toEqual({ insertId: 0, title: 'Estudar JS', status: 'pendente', userId: 0 });
   });
 
 });
