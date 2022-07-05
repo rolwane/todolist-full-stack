394adf2e14ff9a49d827a313f45cb5a209ba41e2
rolwane <rolwane@hotmail.com>
2022-07-05 08:25:39 -0300

    feat: delete function is ok

--- frontend/src/components/TaskList/TaskList.jsx
+++ frontend/src/components/TaskList/TaskList.jsx
@@ -1,19 +1,34 @@
 import React, { useContext } from 'react';
+import { RiDeleteBin6Fill } from 'react-icons/ri';
+import axios from 'axios';
 import AppContext from '../../context/AppContext';
 
 function TaskList() {
-  const { tasks } = useContext(AppContext);
+  const { tasks, setTasks } = useContext(AppContext);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_API_URL}/tasks/${id}`);
+      const remainingTasks = tasks.filter((task) => task.id !== id);
+      setTasks(remainingTasks);
+    } catch (error) {
+      global.console.log(error);
+    }
+  };
 
   return (
     <ul className="p-7">
       {
-        tasks.map((task) => (
+        tasks.map(({ id, title }) => (
           <li
-            className="w-full mb-4 border rounded-lg border-slate-200 p-3 flex justify-between hover:bg-slate-50"
-            key={task.id}
+            className="w-full mb-4 border border-slate-200 p-4 rounded-md flex justify-between hover:bg-slate-50"
+            key={id}
           >
-            {task.title}
-            <button type="button">Excluir</button>
+            {title}
+
+            <button type="button" onClick={() => handleDelete(id)}>
+              <RiDeleteBin6Fill className="text-2xl text-red-500" />
+            </button>
           </li>
         ))
       }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
 // imported components
+import { VscLoading } from 'react-icons/vsc';
 import Header from '../../components/Header/Header';
 import Form from '../../components/Form/Form';
 import TaskList from '../../components/TaskList/TaskList';
@@ -11,7 +12,10 @@ import AppContext from '../../context/AppContext';
 function Home() {
   const { setTasks } = useContext(AppContext);
 
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     const token = localStorage.getItem('token');
 
     axios.get(`${process.env.REACT_APP_API_URL}/tasks`, {
@@ -21,14 +25,19 @@ function Home() {
         Authorization: token,
       },
 
-    }).then((response) => setTasks(response.data));
+    }).then((response) => {
+      setTasks(response.data);
+      setLoading(false);
+    });
   }, []);
 
   return (
     <section className="">
       <Header />
       <Form />
-      <TaskList />
+      {(loading && <VscLoading className="animate-spin text-6xl text-blue-600 m-auto mt-[150px]" />) || (
+        <TaskList />
+      )}
     </section>
   );
 }
